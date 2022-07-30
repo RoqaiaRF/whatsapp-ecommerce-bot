484be936727cf8a44f2fbd1248e75c8330a12080
RoqaiaRF <qoraia97@gmail.com>
2022-07-30 14:51:50 +0300

    test 2

--- sample.env.js
+++ sample.env.js
@@ -8,7 +8,7 @@ const development = {
     NODE_ENV: process.env.NODE_ENV || 'development',
     PORT: '9000',
     Meta_WA_VerifyToken: 'RoqaiaYaqaRoro',
-    Meta_WA_accessToken: 'EAAIIoQNF80YBAGb5OZBiwCvXjI3g2zEr3E1YZBZCzCTSxFvhQsnDGOL9HeWVQpQZAeX6vyGlfBEKZBo0phpqbUkmyWxgzSGk0T4aSuNlyZACO5MiiqtAImmysc959rdDNgGMBLv14tgr3ArYeO7kVZBk0ZCozZAvDdoFjAHVfOWZBRTpXWzahnG8mVYYrNZCZBTszPIBEWSn2YkntX5IVYGG7JeG',
+    Meta_WA_accessToken: 'EAAIIoQNF80YBAJaZA2H66i3SSULrLNntM50sduykZB6LZA0ugHqbdtR1OFRsaiC7GzpQbQcPVIrpCiTel1DfrRGhxT1BcUOvHECQrnC7cBTDTeidqJeZCrTfDCRB096Yd0RfHYbnEM1uQhETvefgzHAwAIaVJKMPBjfVJKpsVE5ZBZCOZBxOyRVBFpnA4hplZCT120HZAO1gdXeaX68X9RMb7',
     Meta_WA_SenderPhoneNumberId: '109406781858892', 
     Meta_WA_wabaId: '104951692313063',
 };
